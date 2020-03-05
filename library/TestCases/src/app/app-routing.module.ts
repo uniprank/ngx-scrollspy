6da70df6f7047a804b2bb37dfc27902920cad2c9
Dennis Fiedler <dennis.fiedler@proximity.com>
2020-03-05 19:34:57 +0100

    refactor: upgrade to angular cli 8

--- library/TestCases/src/app/app-routing.module.ts
+++ library/TestCases/src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
-    { path: 'test-case-1', loadChildren: './modules/test-case1/test-case1.module#TestCase1Module' },
-    { path: 'test-case-2', loadChildren: './modules/test-case2/test-case2.module#TestCase2Module' },
-    { path: 'test-case-3', loadChildren: './modules/test-case3/test-case3.module#TestCase3Module' },
-    { path: 'test-case-4', loadChildren: './modules/test-case4/test-case4.module#TestCase4Module' },
-    { path: 'test-case-5', loadChildren: './modules/test-case5/test-case5.module#TestCase5Module' },
+    { path: 'test-case-1', loadChildren: () => import('./modules/test-case1/test-case1.module').then(m => m.TestCase1Module) },
+    { path: 'test-case-2', loadChildren: () => import('./modules/test-case2/test-case2.module').then(m => m.TestCase2Module) },
+    { path: 'test-case-3', loadChildren: () => import('./modules/test-case3/test-case3.module').then(m => m.TestCase3Module) },
+    { path: 'test-case-4', loadChildren: () => import('./modules/test-case4/test-case4.module').then(m => m.TestCase4Module) },
+    { path: 'test-case-5', loadChildren: () => import('./modules/test-case5/test-case5.module').then(m => m.TestCase5Module) },
     { path: '', redirectTo: '/test-case-1', pathMatch: 'full' }
 ];
 
